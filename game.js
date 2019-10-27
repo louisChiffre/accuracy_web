497972ac72ba9715cb55b7709acbc15996b4f225
Laurent Mauron <laurent.mauron@gmail.com>
2019-10-27 15:45:06 +0100

    Fix typo

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const CONFIGS = [free_config, blob_config, circle_config, square_config]
+//const CONFIGS = [free_config, blob_config, circle_config, square_config]
 const CONFIGS = [blob_config, circle_config, square_config]
 const GAME_NAMES  = CONFIGS.map(x=>x.name).concat(['All']);
 
